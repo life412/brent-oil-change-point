2516ad2e39ee573a82cdd5d685593652ea305b53
Hiwot Alemayehu <151120526+life412@users.noreply.github.com>
2026-02-08 16:51:25 +0300

    Update App.jsx

--- dashboard/frontend/src/App.jsx
+++ dashboard/frontend/src/App.jsx
@@ -1,1 +1,16 @@
 
+import React from "react";
+import PriceChart from "./components/PriceChart";
+import EventHighlights from "./components/EventHighlights";
+
+function App() {
+  return (
+    <div>
+      <h1>Brent Oil Price Dashboard</h1>
+      <PriceChart />
+      <EventHighlights />
+    </div>
+  );
+}
+
+export default App;
